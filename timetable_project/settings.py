e77acdc51df69c31077de703b7fff212fa174dd6
Shruti Shitole <shrutishitole@example.com>
2026-03-06 14:13:36 +0530

    Update settings to use environment variables and add .gitignore

--- timetable_project/settings.py
+++ timetable_project/settings.py
@@ -12,16 +12,20 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Load environment variables from .env file
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8153y#2&ia3yfdrdh%w9)a=22j*)jejc(1!_o1n64y(7cq)xts'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -84,7 +88,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'timetable_db',
         'USER': 'root',
-        'PASSWORD': 'Shrutishitole@20', # <-- Replace with your real password
+        'PASSWORD': os.getenv('DB_PASSWORD'), # <-- Loaded from .env
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
